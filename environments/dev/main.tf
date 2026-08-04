f8f921cae1e9ff73c3df2826d1879a79d2188fe0
Mustafa Tarek <mustafatarek104@gmail.com>
2026-08-04 20:07:50 +0300

    Modify the secrets part

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region = var.region
-  profile = "gym"
-}
-
 module "vpc" {
   source      = "../../modules/vpc"
   azs         = var.azs
@@ -11,7 +6,7 @@ module "vpc" {
 
 module "eks" {
   source            = "../../modules/eks"
-  cluster_name      = "gym-cluster-dev"
+  cluster_name      = "gym-cluster"
   vpc_id            = module.vpc.vpc_id
   private_subnets   = module.vpc.private_subnets
   public_subnets    = module.vpc.public_subnets
@@ -24,40 +19,16 @@ module "ecr" {
   ]
 }
 
+module "secrets" {
+  source       = "../../modules/secrets"
+}
+
 module "auth_rds" {
-  source                  = "../../modules/rds"
+  source                   = "../../modules/rds"
   service_name             = "auth"
-  db_name                  = "auth_db"
+  db_name                  = "gym_auth"
   vpc_id                   = module.vpc.vpc_id
   private_subnets          = module.vpc.private_subnets
   node_security_group_id   = module.eks.node_security_group_id
-}
-
-provider "helm" {
-  kubernetes = {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["--profile", "gym", "eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
-    }
-  }
-}
-
-module "irsa_roles" {
-  source             = "../../modules/irsa-roles"
-  cluster_name       = module.eks.cluster_name
-  oidc_provider_arn  = module.eks.oidc_provider_arn
-  oidc_provider_url  = module.eks.oidc_provider_url
-  depends_on = [module.eks]
-}
-
-module "helm_addons" {
-  source                     = "../../modules/helm-addons"
-  cluster_name               = module.eks.cluster_name
-  vpc_id                     = module.vpc.vpc_id
-  alb_controller_role_arn    = module.irsa_roles.alb_controller_role_arn
-  external_secrets_role_arn  = module.irsa_roles.external_secrets_role_arn
-  depends_on = [module.eks, module.irsa_roles]
+  password                 = module.secrets.auth_db_password
 }
