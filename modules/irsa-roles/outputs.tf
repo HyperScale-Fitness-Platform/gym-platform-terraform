24f93418a7bc87e331c861200309b9101fe5162f
Mustafa Tarek <mustafatarek104@gmail.com>
2026-08-08 18:15:03 +0300

    fix: add kubernetes ebs addon for EKS

--- modules/irsa-roles/outputs.tf
+++ modules/irsa-roles/outputs.tf
@@ -4,6 +4,6 @@ output "alb_controller_role_arn" {
 output "external_secrets_role_arn" {
   value = aws_iam_role.external_secrets.arn
 }
-output "ebs_csi_iam_role_arn" {
+output "ebs_csi_role_arn" {
   value = module.ebs_csi_irsa_role.iam_role_arn
 }
