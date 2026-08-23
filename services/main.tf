8ab8be6d5fdce409fe26f43aabc57b92e86294a7
Mustafa Tarek <mustafatarek104@gmail.com>
2026-08-23 06:22:20 +0300

    Add cert manager

--- services/main.tf
+++ services/main.tf
@@ -104,7 +104,6 @@ resource "helm_release" "cert_manager" {
   name             = "cert-manager"
   repository       = "https://charts.jetstack.io"
   chart            = "cert-manager"
-  version          = "v1.16.2"
   namespace        = "cert-manager"
   create_namespace = true
 
